b53d873f781c076cd238d8f9b499f635cc4ad718
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-04-14 00:41:13 +0500

    fix(components): case sansitivity error resolved

--- Components/index.js
+++ Components/index.js
@@ -1,5 +1,5 @@
 import SubHeading from './SubHeading/SubHeading';
-import Newsletter from './Footer/Newsletter';
+import Newsletter from './Footer/NewsLetter';
 import FooterOverlay from './Footer/FooterOverlay';
 import MenuItem from './MenuItem/MenuItem';
 import Navbar from './Navbar/Navbar';
